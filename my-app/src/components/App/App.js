2d0552fcc06e1f83e6083f5d4f4f07afda45b9da
alicefirminger <alicefirminger@gmail.com>
2023-04-21 15:54:43 +0100

    did some css styling

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,24 +1,23 @@
-import './App.css';
-import RecipeText from '../RecipeText/index';
-import React, { useState } from 'react';
+import "./App.css";
+import RecipeText from "../RecipeText/index";
+import React, { useState } from "react";
 
 function App() {
-  const [showRecipe, setShowRecipe] = useState(false);
+	const [showRecipe, setShowRecipe] = useState(false);
 
-  function handleClick() {
-    setShowRecipe(true);
-    console.log('Button clicked');
-  }
+	function handleClick() {
+		setShowRecipe(true);
+		console.log("Button clicked");
+	}
 
-  return (
-    <div className="App">
-      <button onClick={handleClick}>Press me</button>
-      {showRecipe && <RecipeText setShowRecipe={setShowRecipe} />}
-    </div>
-  );
+	return (
+		<div className="App">
+			<button onClick={handleClick}>
+				Surprise me with a new recipe
+			</button>
+			{showRecipe && <RecipeText setShowRecipe={setShowRecipe} />}
+		</div>
+	);
 }
 
 export default App;
-
-
-
--- my-app/src/components/RecipeText/index.js
+++ my-app/src/components/RecipeText/index.js
@@ -1,38 +1,42 @@
 import { useState, useEffect } from "react";
 import React from "react";
 
-function RecipeText({setShowRecipe}) {
-    const [recipe, setRecipe] = useState("");
-    const [instructions, setInstructions] = useState("");
-    const [image, setImage] = useState(null);
-    useEffect(() => {
-        async function getRecipe() {
-        const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
-        const data = await response.json();
-        setRecipe(data.meals[0].strMeal)
-        setInstructions(data.meals[0].strInstructions)
-        setImage(data.meals[0].strMealThumb)
-        console.log('RecipeText component rendered');
+function RecipeText({ setShowRecipe }) {
+	const [recipe, setRecipe] = useState("");
+	const [instructions, setInstructions] = useState("");
+	const [image, setImage] = useState(null);
+	useEffect(() => {
+		async function getRecipe() {
+			const response = await fetch(
+				"https://www.themealdb.com/api/json/v1/1/random.php"
+			);
+			const data = await response.json();
+			setRecipe(data.meals[0].strMeal);
+			setInstructions(data.meals[0].strInstructions);
+			setImage(data.meals[0].strMealThumb);
+			console.log("RecipeText component rendered");
 
-        // console.log(data.meals[0].strMeal)
-        // console.log(data.meals[0].strInstructions)
-        // console.log(data.meals[0].strMealThumb)
+			// console.log(data.meals[0].strMeal)
+			// console.log(data.meals[0].strInstructions)
+			// console.log(data.meals[0].strMealThumb)
+		}
 
-             }
-             
-      getRecipe();
-    }, []);
-  
-    return (
-      <div className="recipe-text">
-        <h2>Recipe Instructions:</h2>
-        <p>{recipe}</p>
-        <p>{instructions}</p>
-        <img src = {image} alt="img of meal"/>
-        <button onClick={() => setShowRecipe(false)}>Close Recipe</button>
-      </div>
-    );
+		getRecipe();
+	}, []);
+
+	return (
+		<div className="recipe-text">
+			<h1>{recipe}</h1>
+			<img
+				src={image}
+				alt="img of meal"
+				style={{ width: "400px", height: "auto" }}
+			/>
+			<div className="recipe-container"><h2>Recipe Instructions:</h2>
+			<p className="instructions">{instructions}</p></div>
+			<button className="close-button" onClick={() => setShowRecipe(false)}>Close Recipe</button>
+		</div>
+	);
 }
 
 export default RecipeText;
-   
